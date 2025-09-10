8227f9c28e060f39941be520922925dae17ae4fd
vlad <vladpanich48@icloud.com>
2025-09-10 13:17:14 +0300

    Новое исправление

--- source/factorial.js
+++ source/factorial.js
@@ -10,7 +10,6 @@
  * @example
  * factorial(5); // Возвращает 120
  */
-
 const factorial = n => {
     if (!Number.isInteger(n) || n < 0) {
         throw new Error('Факториал определен только для целых неотрицательных чисел');
